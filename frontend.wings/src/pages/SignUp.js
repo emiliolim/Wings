d76635a178c1944d2b0c253d9cdab2f3035942a2
Angie Xetey <axetey@uci.edu>
2025-05-24 18:50:22 -0700

    finished sign up page

--- frontend.wings/src/pages/SignUp.js
+++ frontend.wings/src/pages/SignUp.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import park from "../images/aldrich-park.jpg";
 import "../styles/SignUp.css";
-import {WingsIcon} from '../components/icons'
-import { useNavigate } from 'react-router-dom';
+import { WingsIcon } from "../components/icons";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [username, setUsername] = useState("");
@@ -23,56 +23,82 @@ const SignUp = () => {
   };
 
   return (
-    <div className="logInPage">
-      <section className="top-bar">
-      <div className="WingsLogo" onClick={() => {navigate("/")}} style={{ cursor: "pointer" }}>
-          <WingsIcon />
-          <p className="WingsText">Wings</p>
+    <main className="main_container">
+      {/* Left Side */}
+      <section className="left-grid">
+        <section className="top-bar">
+          <div
+            className="WingsLogo"
+            onClick={() => {
+              navigate("/");
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            <WingsIcon />
+            <p className="WingsText">Wings</p>
           </div>
-          <button className="LoginSignUp">
-            Login/Sign up
+          <button
+            className="LoginSignUp"
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Back to Login
           </button>
         </section>
-      <form onSubmit={handleSubmit} className="formContainer">
-        <label className="formLabel">Username</label>
-        <input
-          type="text"
-          className="formInput"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter username"
-        />
 
-        <label className="formLabel">Email</label>
-        <input
-          type="email"
-          className="formInput"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter email"
-        />
+        <section className="middle-bar">
+          <form onSubmit={handleSubmit} className="formContainer">
+            <label className="formLabel">Username</label>
+            <input
+              type="text"
+              className="formInput"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter username"
+            />
 
-        <label className="formLabel">Password</label>
-        <input
-          type="password"
-          className="formInput"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter password"
-        />
+            <label className="formLabel">Email</label>
+            <input
+              type="email"
+              className="formInput"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+            />
 
-        <label className="formLabel">Confirm Password</label>
-        <input
-          type="password"
-          className="formInput"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          placeholder="Confirm password"
-        />
+            <label className="formLabel">Password</label>
+            <input
+              type="password"
+              className="formInput"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+            />
 
-        <button type="submit" className="submitButton">Sign Up</button>
-      </form>
-    </div>
+            <label className="formLabel">Confirm Password</label>
+            <input
+              type="password"
+              className="formInput"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Confirm password"
+            />
+
+            <button type="submit" className="submitButton">
+              Sign Up
+            </button>
+          </form>
+        </section>
+      </section>
+
+      {/* Right Side */}
+      <section className="right-grid">
+        <div>
+          <img src={park} alt="Aldrich Park" />
+        </div>
+      </section>
+    </main>
   );
 };
 
