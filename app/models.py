10816a6f976e5ea4a4907c5c9a671ab424e4e524
emiliolim <emilioarislim@gmail.com>
2025-05-24 20:06:45 -0700

    small code cleanup, import jwt

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,18 +8,12 @@ class Users(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
-    # def set_password(self, password):
-    #     self.password_hash = generate_password_hash(password)
-    
-    # def check_password(self, password):
-    #     return check_password_hash(self.password_hash, password)
-    @property
-    def password(self):
-        raise AttributeError('Password is not readable')
-    
-    @password.setter
-    def password(self, plaintext_password):
-        self.password_hash = generate_password_hash(plaintext_password)
+    def generate_auth_token(self, expiration=600):
+    {
+        return jwt.encode(
+            {'id': self.id, 'exp': datetime.datetime.now(datetime.)}
+        )
+    }
 
     def __repr__(self):
         return f'<User {self.username}>'
