189f9040d1ce6a15403b4d27f84a39f5a1476f0b
emiliolim <emilioarislim@gmail.com>
2025-05-25 09:02:17 -0700

    api

--- frontend.wings/src/Api.js
+++ frontend.wings/src/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL;
+export const API_URL = process.env.API_URL;
 export const get_users = async () =>
 {
     const response = await axios.get(`${API_URL}/users`);
